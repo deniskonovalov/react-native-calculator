eb86ab787f175133506ccb15b0b7c840d558d43d
Denis Konovalov <denis.konov93@gmail.com>
2018-12-16 21:15:27 +0200

    Initial Commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+        StyleSheet, 
+        Text
+      } from 'react-native';
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
